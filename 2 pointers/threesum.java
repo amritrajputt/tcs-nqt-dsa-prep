9fd10911f18048f0eb58373cf5ae491eeec6e273
amrit <vishal179singh@gmail.com>
2026-05-26 21:00:38 +0530

    Refactor threeSum solution to find all triplets that sum to zero

--- 2 pointers/threesum.java
+++ 2 pointers/threesum.java
@@ -1,19 +1,35 @@
-// Find all triplets in the array that sum to zero.  
-// `Input: [-1,0,1,2,-1,-4]` → `Output: [[-1,-1,2],[-1,0,1]]`
+import java.util.*;
+class threeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        // i ko fix loop me kr dete hai i ki last 3rd tk hi ja skta hai
+        for (int i = 0; i <= nums.length - 3; i++) {
+            // agar current ele of i same hai prev ele k to skip krte jaao
+            if (i != 0 && nums[i] == nums[i - 1])
+                continue;
 
-import java.util.Scanner;
-
-public class threesum {
-    public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);
-        System.out.print("enter size of array: ");
-        int n = sc.nextInt();
-        System.out.print("enter target no: ");
-        int target = sc.nextInt();
-        int arr[] = new int[n];
-        System.out.print("enter elements of array: ");
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
+            int j = i + 1, k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum > 0)
+                    k--;
+                else if (sum < 0)
+                    j++;
+                else {
+                    List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
+                    result.add(temp);
+                    j++;
+                    k--;
+                    // agar current ele of j same hai prev ele k to skip krte jaao
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
+                    // agar current ele of k same hai prev ele k to skip krte jaao
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--;
+                }
+            }
         }
+        return result;
     }
 }
