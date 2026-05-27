3165ba145671f3130d3188c3c6a42d3cab044fa7
amrit <vishal179singh@gmail.com>
2026-05-27 19:50:37 +0530

    Add PalindromeCheck solution to check if a string is a palindrome

---
+++ 2 pointers/PalindromeCheck.java
@@ -0,0 +1,16 @@
+public class PalindromeCheck {
+    public static boolean checkPalindrome(String s) {
+        if (s == null || s.length() <= 1)
+            return true;
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j))
+                return false;
+            else {
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+}
